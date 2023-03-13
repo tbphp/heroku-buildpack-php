8a445a5332244249412b80c6dbc4232e8fc219c3
tangbo <admin@tbphp.net>
2023-03-13 21:42:34 +0800

    zmq

--- bin/zmq.sh
+++ bin/zmq.sh
@@ -5,8 +5,9 @@ echo 'start install zmq'
 #apt-get update -qq || { echo "Failed to 'apt-get update'. You must build this formula using Docker."; exit 1; }
 #apt-get install -q -y libzmq3-dev
 
+
+/app/.heroku/php-min/bin/php -i |grep php.ini
 BUILD_DIR=$1
-echo $PATH
 exit
 echo "extension=zmq.so" >> /app/.heroku/php/etc/php/php.ini
 echo 'zmq done'
